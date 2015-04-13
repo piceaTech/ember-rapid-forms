2e883ef12939a2bc0d530411f489430347ea8ecb
Felix Fichte <dev@spruce.de>
2015-04-14 01:38:19 +0200

    preparation for ember-rapid-forms gh-pages added me as author + reflect name change

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/ember-forms',
+    baseURL: '/ember-rapid-forms',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
