d843eb5076a6fe5546093d5d871bf1a0e3348234
Pedro Kiefer <pedro@kiefer.com.br>
2015-05-05 11:22:49 -0300

    Opppss... Error while margin ember-rapid-forms

--- addon/mixins/control.js
+++ addon/mixins/control.js
@@ -8,13 +8,7 @@ export default Ember.Mixin.create({
   "class": 'form-control',
   init: function() {
     this._super();
-
-    var propertyIsModel = this.get('parentView.propertyIsModel');
-    if(propertyIsModel) {
-    	return Em.Binding.from("model" + '.' + (this.get('propertyName')) + '.content').to('selection').connect(this); 
-    } else {
-    	return Em.Binding.from("model" + '.' + (this.get('propertyName'))).to('value').connect(this);    	
-    }
+    return Ember.Binding.from("model." + (this.get('propertyName'))).to('value').connect(this);
   },
   hasValue: Ember.computed.readOnly('value', function() {
     return this.get('value') !== null;
