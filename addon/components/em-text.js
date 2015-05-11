7063642fc840791ae4db43a736fdd09d49c3d1f6
Pedro Kiefer <pedro@kiefer.com.br>
2015-05-11 10:59:21 -0300

    Add disabled option Fixes cols and rows attributes Add basic unit tests for textarea widget

--- addon/components/em-text.js
+++ addon/components/em-text.js
@@ -3,23 +3,26 @@ import FormGroupComponent from './em-form-group';
 import ControlMixin from 'ember-rapid-forms/mixins/control';
 
 /*
-Form Input
+Form Text Area
 
 Syntax:
-{{em-text property="property name" rows=4}}
+{{em-text property="property name" rows=4 cols=40}}
  */
 export default FormGroupComponent.extend({
   controlView: Ember.TextArea.extend(ControlMixin, {
-    attributeBindings: ['placeholder'],
     placeholder: Ember.computed.alias('mainComponent.placeholder'),
     model: Ember.computed.alias('mainComponent.model'),
     propertyName: Ember.computed.alias('mainComponent.propertyName'),
-    rows: Ember.computed.alias('mainComponent.rows')
+    rows: Ember.computed.alias('mainComponent.rows'),
+    cols: Ember.computed.alias('mainComponent.cols'),
+    disabled: Ember.computed.alias('parentView.disabled')
   }),
-  property: void 0,
-  label: void 0,
-  placeholder: void 0,
-  rows: 4,
+  property: null,
+  label: null,
+  placeholder: null,
+  rows: null,
+  cols: null,
+  disabled: null,
   controlWrapper: Ember.computed('form.form_layout', function() {
     if (this.get('form.form_layout') === 'horizontal') {
       return 'col-sm-10';
