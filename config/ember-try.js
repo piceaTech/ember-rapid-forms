a1dea57aa3dd1a80160f93a629532d332e7584b9
Pedro Kiefer <pedro@kiefer.com.br>
2015-05-21 12:01:31 -0300

    Update to ember-cli@0.2.5

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,36 +1,34 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.11.3',
-      dependencies: {
-        "ember": "1.11.3"
-      }
+      name: 'default',
+      dependencies: { }
     },
     {
       name: 'ember-release',
       dependencies: {
-        "ember": "components/ember#release"
+        'ember': 'components/ember#release'
       },
       resolutions: {
-        "ember": "release"
+        'ember': 'release'
       }
     },
     {
       name: 'ember-beta',
       dependencies: {
-        "ember": "components/ember#beta"
+        'ember': 'components/ember#beta'
       },
       resolutions: {
-        "ember": "beta"
+        'ember': 'beta'
       }
     },
     {
       name: 'ember-canary',
       dependencies: {
-        "ember": "components/ember#canary"
+        'ember': 'components/ember#canary'
       },
       resolutions: {
-        "ember": "canary"
+        'ember': 'canary'
       }
     }
   ]
