52303b9bacaa57967aa2dae2896e72b47373b03b
Felix Fichte <dev@spruce.de>
2015-08-20 15:39:24 +0200

    update ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,6 @@
 /* global require, module */
+var mergeTrees = require('broccoli-merge-trees');
+var funnel = require('broccoli-funnel');
 var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
@@ -12,6 +14,20 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+  app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css.map');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/fontawesome/css/font-awesome.min.css');
+  app.import('bower_components/highlightjs/highlight.pack.js');
+  app.import('bower_components/highlightjs/styles/tomorrow.css');
+  app.import('bower_components/ember/ember-template-compiler.js');
+  
+  var extraAssets = funnel('bower_components/fontawesome/fonts', {
+      srcDir  : '/',
+      files   : [ 'fontawesome-webfont.woff' ],
+      destDir : '/fonts'
+  });
+  
+  return  mergeTrees([app.toTree(), extraAssets]);
 
   return app.toTree();
 };
