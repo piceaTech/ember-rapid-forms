2ad7cd6d94b5109dc2ab8bee17ede478f81e6ddc
Felix Fichte <dev@spruce.de>
2015-10-04 15:57:14 +1100

    fix most of select tests
    ping #54

--- tests/integration/components/html-select-test.js
+++ tests/integration/components/html-select-test.js
@@ -1,23 +1,51 @@
-// import {
-//   moduleForComponent, test
-// }
-// from 'ember-qunit';
-// import hbs from 'htmlbars-inline-precompile';
+// TODO: Remove with ember 2.0
+import Ember from 'ember';
+var originalEmberDeprecate = Ember.deprecate; // we know it is deprecated but can't do anything about it
+Ember.deprecate = function(message) {
+  if(message !== 'Using Ember.HTMLBars._registerHelper is deprecated. Helpers (even dashless ones) are automatically resolved.' &&  message !== 'ember-get-helper has been included in Ember 2.0. Use of this package is deprecated.'){
+    originalEmberDeprecate.apply(this, arguments);
+  }
+  else{
+    console.log('removing deprecation:', message);
+  }
+};
+// end TODO
+import {
+  moduleForComponent, test
+}
+from 'ember-qunit';
 
-// moduleForComponent('html-select', 'Integration | Component | html select', {
-//   integration: true
-// });
 
-// test('it renders', function(assert) {
-//   assert.expect(1);
 
-//   // Set any properties with this.set('myProperty', 'value');
-//   // Handle any actions with this.on('myAction', function(val) { ... });
-//   console.log(requirejs.entries);
-//   this.render(hbs `{{erf-html-select}}`);
+import hbs from 'htmlbars-inline-precompile';
 
-//   assert.equal(this.$().text().trim(), '');
+// TODO: remove when upgrading to ember 2.0
+import { registerHelper } from 'ember-get-helper/utils/register-helper';
 
-//   // Template block usage:
-//   // not applicable
-// });
+// if you are on Ember 1.13
+import getHelper from 'ember-get-helper/helpers/get-glimmer';
+
+// register it before your tests (this could be in the beforeEach, but it doesn't need to be)
+
+registerHelper('get', getHelper);
+// end TODO
+
+moduleForComponent('html-select', 'Integration | Component | html select', {
+  integration: true,
+  afterEach: function(){
+    Ember.deprecate = originalEmberDeprecate;
+  }
+});
+
+test('it renders', function(assert) {
+  assert.expect(1);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs `{{erf-html-select}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  // not applicable
+});
--- tests/unit/components/em-select-test.js
+++ tests/unit/components/em-select-test.js
@@ -1,123 +1,140 @@
-// import {
-//   moduleForComponent,
-//   test
-//   } from 'ember-qunit';
-// import Ember from 'ember';
-// import hbs from 'htmlbars-inline-precompile';
+import {
+  moduleForComponent,
+  test
+}
+from 'ember-qunit';
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
 
-// var fruitSalad = null;
+// TODO: Remove with ember 2.0
+var originalEmberDeprecate = Ember.deprecate; // we know it is deprecated but can't do anything about it
+const ownDeprecate = function(message) {
+  if (message !== 'Using Ember.HTMLBars._registerHelper is deprecated. Helpers (even dashless ones) are automatically resolved.' && message !== 'ember-get-helper has been included in Ember 2.0. Use of this package is deprecated.') {
+    originalEmberDeprecate.apply(this, arguments);
+  } else {
+    console.log('removing deprecation:', message);
+  }
+};
 
-// moduleForComponent('em-select', {
-//   // Specify the other units that are required for this test
-//   integration: true,
-//   beforeEach() {
-//     fruitSalad = Ember.Object.create({
-//       favoriteFruit: null,
-//       fruits: Ember.Object.create(),
-//       errors: Ember.Object.create(),
-//       validate() {
-//         var promise = new Ember.RSVP.Promise((resolve) => {
-//           resolve('ok!');
-//         });
-//         return promise;
-//       }
-//     });
-//   },
-//   afterEach() {
-//     Ember.run(() => {
-//       fruitSalad.destroy();
-//     });
-//   }
-// });
+// TODO: remove when upgrading to ember 2.0
+import {
+  registerHelper
+}
+from 'ember-get-helper/utils/register-helper';
+
+// if you are on Ember 1.13
+import getHelper from 'ember-get-helper/helpers/get-glimmer';
+
+// register it before your tests (this could be in the beforeEach, but it doesn't need to be)
+
+registerHelper('get', getHelper);
+// end TODO
 
 
-// var fruitOptions = Ember.A([
-//   {id: 1, name: 'Banana'},
-//   {id: 2, name: 'Pineapple'},
-//   {id: 3, name: 'Orange'},
-//   {id: 4, name: 'Apple'}
-// ]);
 
-// test('em-select renders', function(assert) {
-//   this.set('fruitOptions', fruitOptions);
+var fruitSalad = null;
 
-//   this.render(hbs`{{em-select label="Fruits" content=fruitOptions optionValuePath='content.id' optionLabelPath='content.name'}}`);
+moduleForComponent('em-select', {
+  // Specify the other units that are required for this test
+  integration: true,
+  beforeEach() {
+    Ember.deprecate = ownDeprecate;
+    fruitSalad = Ember.Object.create({
+      favoriteFruit: null,
+      fruits: Ember.Object.create(),
+      errors: Ember.Object.create(),
+      validate() {
+        var promise = new Ember.RSVP.Promise((resolve) => {
+          resolve('ok!');
+        });
+        return promise;
+      }
+    });
+  },
+  afterEach() {
+    Ember.deprecate = originalEmberDeprecate;
+    Ember.run(() => {
+      fruitSalad.destroy();
+    });
+  }
+});
 
-//   var element = this.$();
 
-//   assert.equal(element.find('label:contains("Fruits:")').length, 1, 'label has for property');
+var fruitOptions = Ember.A([{
+  id: 1,
+  name: 'Banana'
+}, {
+  id: 2,
+  name: 'Pineapple'
+}, {
+  id: 3,
+  name: 'Orange'
+}, {
+  id: 4,
+  name: 'Apple'
+}]);
 
-//   var select = element.find('select')[0];
-//   assert.ok(select, 'select exists');
+test('em-select renders', function(assert) {
+  this.set('fruitOptions', fruitOptions);
 
-//   fruitOptions.forEach((item, index) => {
-//     assert.equal(select.options[index].value, item.id, 'Id '+ item.id +' exists on select');
-//     assert.equal(select.options[index].text, item.name, 'Name '+ item.name +' exists on select');
-//   });
-// });
+  this.render(hbs `{{em-select label="Fruits:" content=fruitOptions optionValuePath='id' optionLabelPath='name'}}`);
 
-// test('em-select renders with a prompt', function(assert) {
+  var element = this.$();
 
-//   var component = this.subject({
-//     label: 'Fruits:',
-//     content: fruitOptions,
-//     optionValuePath: 'content.id',
-//     optionLabelPath: 'content.name',
-//     prompt: 'None'
-//   });
+  assert.equal(element.find('label:contains("Fruits:")').length, 1, 'label has for property');
 
-//   this.render();
-//   var element = this.$();
+  var select = element.find('select')[0];
+  assert.ok(select, 'select exists');
 
-//   assert.equal(element.find('label:contains("Fruits:")').length, 1, 'label has for property');
+  fruitOptions.forEach((item, index) => {
+    assert.equal(select.options[index].value, item.id, 'Id ' + item.id + ' exists on select');
+    assert.equal(select.options[index].text, item.name, 'Name ' + item.name + ' exists on select');
+  });
+});
 
-//   var select = element.find('select')[0];
-//   assert.ok(select, 'select exists');
-//   assert.equal(select.options[0].text, 'None', 'Prompt gets rendered');
-// });
+test('em-select renders with a prompt', function(assert) {
 
-// test('em-select can select an item', function(assert) {
+  this.set('fruitOptions', fruitOptions);
 
-//   var component = this.subject({
-//     label: 'Fruits:',
-//     content: fruitOptions,
-//     propertyName: 'favoriteFruit',
-//     optionValuePath: 'content.id',
-//     optionLabelPath: 'content.name',
-//     model: fruitSalad
-//   });
+  this.render(hbs `{{em-select label="Fruits:" content=fruitOptions optionValuePath='id' optionLabelPath='name' prompt='None'}}`);
 
-//   this.render();
-//   var element = this.$();
-//   assert.equal(element.find('label:contains("Fruits:")').length, 1, 'label has for property');
+  var element = this.$();
 
-//   var select = element.find('select')[0];
-//   assert.ok(select.options.length > 3, 'select has options');
+  assert.equal(element.find('label:contains("Fruits:")').length, 1, 'label has for property');
 
-//   Ember.run(() => {
-//     Ember.$(select).val('3');
-//     Ember.$(select).trigger('change');
-//   });
+  var select = element.find('select')[0];
+  assert.ok(select, 'select exists');
+  assert.equal(select.options[0].text, 'None', 'Prompt gets rendered');
+});
 
-//   assert.equal(component.get('selection.name'), 'Orange', 'Orange got selected');
-//   assert.equal(fruitSalad.get('favoriteFruit'), 3, 'model favorite fruit is the selection');
+test('em-select can select an item', function(assert) {
 
-// });
+  this.set('fruitOptions', fruitOptions);
+  this.set('fruitSalad', fruitSalad);
+
+  this.render(hbs `{{em-select label="Fruits:" content=fruitOptions optionValuePath='id' optionLabelPath='name' prompt='None' property='favoriteFruit' model=fruitSalad}}`);
+
+  var element = this.$();
+  assert.equal(element.find('label:contains("Fruits:")').length, 1, 'label has for property');
+
+  var select = element.find('select')[0];
+  assert.ok(select.options.length > 3, 'select has options');
+
+  Ember.run(() => {
+    this.$(select).val('3');
+    this.$(select).trigger('change');
+  });
+  assert.equal(fruitSalad.get('favoriteFruit'), 3, 'model favorite fruit is the selection');
+
+});
 
 // test('em-select can select multiple items', function(assert) {
 
-//   var component = this.subject({
-//     label: 'Fruits:',
-//     content: fruitOptions,
-//     propertyName: 'fruits',
-//     optionValuePath: 'content.id',
-//     optionLabelPath: 'content.name',
-//     model: fruitSalad,
-//     propertyIsModel: true,
-//     multiple: true
-//   });
+//   this.set('fruitOptions', fruitOptions);
+//   this.set('fruitSalad', fruitSalad);
 
-//   this.render();
+//   this.render(hbs `{{em-select label="Fruits:" content=fruitOptions optionValuePath='id' optionLabelPath='name' prompt='None' property='fruits' model=fruitSalad propertyIsModel=true multiple=true}}`);
+
 //   var element = this.$();
 //   assert.equal(element.find('label:contains("Fruits:")').length, 1, 'label has for property');
 
@@ -133,7 +150,13 @@
 //     Ember.$(select).trigger('change');
 //   });
 
-//   assert.deepEqual(component.get('selection'), [{id: 3, name: 'Orange'}, {id: 4, name: 'Apple'}], "Apple and Oranges");
-//   assert.deepEqual(fruitSalad.get('fruits.content'), [{id: 3, name: 'Orange'}, {id: 4, name: 'Apple'}], 'fruits got selected');
+//   console.log(fruitSalad.get('fruits.content'), fruitSalad.get('fruits'));
+//   assert.deepEqual(fruitSalad.get('fruits.content'), [{
+//     id: 3,
+//     name: 'Orange'
+//   }, {
+//     id: 4,
+//     name: 'Apple'
+//   }], 'fruits got selected');
 
 // });
