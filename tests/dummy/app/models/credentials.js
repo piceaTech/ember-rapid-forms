8a645b25c7ac23326e58b73e3b5f38783a404426
Drew Nichols <drew@liftforward.com>
2015-11-09 13:20:55 -0500

    updating demo app to use ember-validations 2.0.0-alpha.4

--- tests/dummy/app/models/credentials.js
+++ tests/dummy/app/models/credentials.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import DS from 'ember-data';
 import EV from 'ember-validations';
 
-var Credentials = DS.Model.extend(EV.Mixin, {
+var Credentials = DS.Model.extend(EV, {
   user: DS.attr('string'),
   password: DS.attr('string'),
   isntValid: Ember.computed.not('isValid')
