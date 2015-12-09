e6fea7a93b84249c4d5968cc4f49e240fd339f20
Agustin Carrasco <asermax@gmail.com>
2015-12-09 00:34:36 -0300

    added tests for cid on different controls

--- tests/unit/components/em-input-test.js
+++ tests/unit/components/em-input-test.js
@@ -38,3 +38,11 @@ test('Input renders with custom css', function(assert) {
   assert.ok(this.$().find('input').parent().parent().hasClass('col-md-6'), 'Input parent has correct class');
   assert.ok(this.$().find('input').hasClass('col-md-6'), 'Input has correct class');
 });
+
+test('cid correctly sets the id for the input and it\'s label', function(assert) {
+  assert.expect(2);
+  this.render(hbs`{{em-input label="some label" cid='test-cid'}}`);
+
+  assert.equal(this.$('input').attr('id'), 'test-cid', 'input has correct id');
+  assert.equal(this.$('label').attr('for'), 'test-cid', 'label has correct \'for\'');
+});
--- tests/unit/components/em-text-test.js
+++ tests/unit/components/em-text-test.js
@@ -6,7 +6,12 @@ import Ember from 'ember';
 
 moduleForComponent('em-text', {
   // Specify the other units that are required for this test
-  needs: ['component:form-group', 'component:form-group-control', 'component:erf-html-text']
+  needs: [
+    'component:form-group',
+    'component:form-group-control',
+    'component:erf-html-text',
+    'component:em-form-label'
+  ]
 });
 
 test('Textarea renders', function(assert) {
@@ -61,4 +66,14 @@ test('Textarea renders with custom css', function(assert) {
 
   this.render();
   assert.ok(Ember.$(component.element).find('textarea').hasClass('col-md-6'), 'Textarea has correct class');
+});
+
+test('cid correctly sets the id for the textarea and it\'s label', function(assert) {
+  assert.expect(2);
+  let cid = 'test-cid';
+  let component = this.subject({label: 'some label', cid: cid});
+  this.render();
+
+  assert.equal(component.$('textarea').attr('id'), cid, 'textarea has correct id');
+  assert.equal(component.$('label').attr('for'), cid, 'label has correct \'for\'');
 });
