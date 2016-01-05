85191553b8bc6061e7e10ab05cd8f3cd56077699
GCorbel <guirec.corbel@gmail.com>
2016-01-05 07:42:46 -0500

    Property is the default cid

--- addon/components/form-group.js
+++ addon/components/form-group.js
@@ -3,5 +3,16 @@ import layout from '../templates/components/form-group';
 
 export default Ember.Component.extend({
   layout: layout,
-  tagName: ''
+  tagName: '',
+  property: null,
+  id: Ember.computed('cid', 'property', {
+    get: function() {
+      if (this.get('cid')) {
+        return this.get('cid');
+      } else {
+        console.log(this.get('property'));
+        return this.get('property');
+      }
+    }
+  }),
 });
