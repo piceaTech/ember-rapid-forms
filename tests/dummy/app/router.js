59e55bdcf0e25b68ba606fc939ecf7831a88d33f
Felix Fichte <dev@spruce.de>
2016-01-13 14:43:12 +0100

    fixing deprecations

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -5,22 +5,25 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map( function() {
-    this.route( 'overview' );
-    this.route( 'getstarted' );
-    this.route( 'quickexample' );
-    this.route( 'form' );
+Router.map(function() {
+  this.route('overview');
+  this.route('getstarted');
+  this.route('quickexample');
+  this.route('form');
 
-    this.resource( 'controls', function() {
-        this.route( 'custom-submit' );
-        this.route( 'input' );
-        this.route( 'text' );
-        this.route( 'checkbox' );
-        this.route( 'select' );
-        this.route( 'html5' );
-        this.route( 'wrapped-input' );
-        this.route( 'custom-styles' );
-    });
+
+  this.route('controls', {
+    resetNamespace: true
+  }, function() {
+    this.route('custom-submit');
+    this.route('input');
+    this.route('text');
+    this.route('checkbox');
+    this.route('select');
+    this.route('html5');
+    this.route('wrapped-input');
+    this.route('custom-styles');
+  });
 });
 
 export default Router;
