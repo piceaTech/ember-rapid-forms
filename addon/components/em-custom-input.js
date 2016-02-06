f41e04d41e8551f2c234faa92cfe0286906ce4a4
GCorbel <guirec.corbel@gmail.com>
2016-02-06 08:47:14 -0500

    Add a custom input component

---
+++ addon/components/em-custom-input.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import FormGroupComponent from './em-form-group';
+
+/*
+Form Input
+
+Syntax:
+{{#em-custom-input property="property name"}}Something{{/em-custom-input}}
+ */
+export default FormGroupComponent.extend({
+  elementClass: null,
+  htmlComponent: 'erf-html-custom-input',
+  property: null,
+  label: null,
+  name: null,
+  placeholder: null,
+  required: null,
+  autofocus: null,
+  disabled: null,
+  controlWrapper: Ember.computed('form.formLayout', {
+    get: function() {
+      if (this.get('form.formLayout') === 'horizontal') {
+        return 'col-sm-10';
+      }
+      return null;
+    }
+  })
+});
