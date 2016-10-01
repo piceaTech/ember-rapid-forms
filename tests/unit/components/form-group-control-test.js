27f871a2fe885c656cc1378eed4c546b10527e15
GCorbel <guirec.corbel@gmail.com>
2016-10-01 12:13:12 -0400

    Fix bug in latest ember versions

--- tests/unit/components/form-group-control-test.js
+++ tests/unit/components/form-group-control-test.js
@@ -3,29 +3,27 @@ import {
   test
 }
 from 'ember-qunit';
-import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('form-group-control', {
   // Specify the other units that are required for this test
-  integration: true
-});
+  integration: true,
 
-var mainComponent = Ember.Component.create({
-  htmlComponent: 'erf-html-input'
+  beforeEach() {
+    const mainComponent = this.container.lookupFactory('component:em-form-group').create({
+      htmlComponent: 'erf-html-input'
+    });
+    this.set('mainComponent', mainComponent);
+  }
 });
 
 test('renders the component', function(assert) {
-  this.set('mainComponent', mainComponent);
-
   this.render(hbs`{{form-group-control mainComponent=mainComponent}}`);
 
   assert.equal(this.$().find('input').length, 1, 'Has htmlComponent rendered');
 });
 
 test('renders the component with wrapper', function(assert) {
-  this.set('mainComponent', mainComponent);
-
   this.render(hbs `{{form-group-control mainComponent=mainComponent controlWrapper='control-wrapper-class'}}`);
 
   assert.equal(this.$().find('input').length, 1, 'Has htmlComponent rendered');
