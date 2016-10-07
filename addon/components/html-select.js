805ff2c2267ea1f5bfc94845f7568a965ec99a2f
Felix Fichte <dev@spruce.de>
2016-10-07 22:40:26 +0200

    Work on using contextual components

--- addon/components/html-select.js
+++ addon/components/html-select.js
@@ -34,7 +34,7 @@ export default Ember.Component.extend({
 
   },
   actions: {
-    change() {
+    change(event) {
 
       const selectedEl = this.$('select')[0];
       let selectedIndex = selectedEl.selectedIndex;
