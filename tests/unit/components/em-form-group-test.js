18304b6af47a967538c3a0443fccf96d2789b616
GCorbel <guirec.corbel@gmail.com>
2016-12-18 19:31:37 -0500

    Add a class when a field is required

--- tests/unit/components/em-form-group-test.js
+++ tests/unit/components/em-form-group-test.js
@@ -89,3 +89,10 @@ test('I18n label is overrided by a given value', function(assert) {
 
   assert.equal(this.$('label').text().trim(), 'Custom Label', 'Doesn\t use custom labels');
 });
+
+test('Add required class if the field is required', function(assert) {
+  this.set('model', Ember.Object.create());
+  this.render(hbs`{{em-form-group model=model required=true}}`);
+
+  assert.ok(this.$('.form-group').hasClass('required'));
+});
