ee6b36aeb210feb4585a2a1b8a91fd26ba2c8802
DC <dan212@gmail.com>
2017-04-22 17:31:07 -0400

    Fixes

--- addon/components/em-checkbox-input.js
+++ addon/components/em-checkbox-input.js
@@ -6,13 +6,17 @@ const { Component, computed } = Ember;
 export default Component.extend({
   layout: layout,
   tagName: '',
-  classNameBindings: ['checkboxWrapper'],
-  didReceiveAttrs() {
-    this._super(...arguments);
-    if (this.get('checkboxWrapper')) {
-      this.set('tagName', 'div');
-    } else {
-      this.set('tagName', '');
-    }
-  },
+  label: computed.alias('checkboxComponent.label'),
+  form: computed.alias('checkboxComponent.form'),
+  inputId: computed.alias('checkboxComponent.inputId'),
+  labelClass: computed.alias('checkboxComponent.labelClass'),
+  model: computed.alias('checkboxComponent.model'),
+  property: computed.alias('checkboxComponent.property'),
+  disabled: computed.alias('checkboxComponent.disabled'),
+  required: computed.alias('checkboxComponent.required'),
+  autofocus: computed.alias('checkboxComponent.autofocus'),
+  readonly: computed.alias('checkboxComponent.readonly'),
+  title: computed.alias('checkboxComponent.title'),
+  elementClass: computed.alias('checkboxComponent.elementClass'),
+  name: computed.alias('checkboxComponent.name')
 });
