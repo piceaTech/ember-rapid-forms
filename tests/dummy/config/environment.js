8c578ece3026ecc6ce9211d0e1bcf1355b47438d
Felix Fichte <dev@spruce.de>
2018-11-08 09:34:56 +0100

    Config no empty block

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -52,9 +52,9 @@ module.exports = function(environment) {
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
+  // if (environment === 'production') {
 
-  }
+  // }
 
   return ENV;
 };
